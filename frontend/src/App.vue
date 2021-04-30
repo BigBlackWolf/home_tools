26f2fa9768525646689bfb3637f0203b84a48da2
BigBlackWolf <stepan.rasputny@gmail.com>
2021-04-30 23:37:19 +0300

    Added frontend boilerplate

---
+++ frontend/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <div>
+      <b-nav tabs align="center">
+        <b-nav-item active>
+          <router-link to="/">Home</router-link>
+        </b-nav-item>
+        <b-nav-item>
+          <router-link to="/login">Login</router-link>
+        </b-nav-item>
+        <b-nav-item>Link with a long name</b-nav-item>
+        <b-nav-item disabled>Disabled</b-nav-item>
+      </b-nav>
+    </div>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
