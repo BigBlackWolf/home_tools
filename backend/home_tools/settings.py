22e13334977f5c226f5af1e5298b704febcd3d54
BigBlackWolf <stepan.rasputny@gmail.com>
2021-05-12 18:57:32 +0300

    Added gunicorn

--- backend/home_tools/settings.py
+++ backend/home_tools/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = "(@!$+5-7e30p0)f3-p7fm=vg)a^$v=b+jk8(b^u!y97y*!%_g9"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "0.0.0.0",
